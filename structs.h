45765fb6a06d28af95b6470571c685c52a401ffb
Umuril <umuril.lyerood@gmail.com>
2015-08-12 12:34:13 +0200

    Add file structs.h
    Version 1.0.3

---
+++ structs.h
@@ -0,0 +1,21 @@
+#include <tuple>	// std::tie
+#include <memory>	// std::shared_ptr
+
+struct node	{
+	unsigned int oper : 3;
+	void *a;
+	void *b;
+	
+	bool operator < (const node & rhs) const {
+		return std::tie(oper,a,b) < std::tie(rhs.oper,rhs.a,rhs.b);	
+	}
+};
+
+struct weighted_pointer {
+	mutable int weight;
+	std::shared_ptr<node> pointer;
+	
+	bool operator < (const weighted_pointer & rhs) const {
+		return pointer < rhs.pointer;
+	}
+};
