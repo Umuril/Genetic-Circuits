f6457bbcf437bb295439ee8738d787283ca0aae5
Umuril <umuril.lyerood@gmail.com>
2015-08-12 22:50:53 +0200

    Add some from old main
    Version 1.0.18

--- main.cc
+++ main.cc
@@ -22,6 +22,48 @@ int main(){
 	std::vector<bool> perfect(output.size());
 	
 	init(input,output,gate,root);
+
+	fitness(root,input,output,gate,perfect);
+	printData(input,output,root,perfect);
+	int bestfitness = 0;
+	int newone = 0;
+	double media = 0;
+	unsigned long long int nums = 0;
+	backup bestsolution;
+	bestsolution.gate.insert(gate.begin(),gate.end());
+	bestsolution.root.assign(root.begin(),root.end());
+	do{
+		randomize(gate,input,root);
+		for_each(gate.begin(),gate.end(),[](weighted_pointer const & w){w.weight = 0;});
+		newone = fitness(root,input,output,gate,perfect);
+		//if(newone > 2000)
+		media = ((media*nums)+newone)/(nums+1);
+		if(nums < 100)
+			nums++;
+		//printf("fitness: %7d media: %7.2f nums: %llu\r",newone,media,nums);
+		if(newone < bestfitness-100){
+			gate.clear();
+			gate.insert(bestsolution.gate.begin(),bestsolution.gate.end());
+			root.assign(bestsolution.root.begin(),bestsolution.root.end());
+		}
+		if(newone > bestfitness){
+			bestsolution.gate.clear();
+			bestsolution.gate.insert(gate.begin(),gate.end());
+			bestsolution.root.assign(root.begin(),root.end());
+			bestfitness = newone;
+		//}
+			system("cls");
+			
+			for(unsigned int i = 0; i < root.size(); i++){
+				if(perfect[i])
+					addWeight(root[i],true,gate);
+				std::cout << " " << perfect[i];
+			}
+			std::cout << "\n";
+			printData(input,output,root,perfect);
+		}
+		//std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+	}while(bestfitness < 2650);
 	
 	return 0;
 }
