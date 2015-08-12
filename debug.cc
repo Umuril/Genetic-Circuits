0d729795d1c023746497d4a76a747b1fe94a49d4
Umuril <umuril.lyerood@gmail.com>
2015-08-13 00:01:59 +0200

    Change little details of code
    Version 1.0.20

--- debug.cc
+++ debug.cc
@@ -3,26 +3,26 @@
 void printRoots(const node *root){
 	if(!root)
 		return;
-	std::cout << root << ": " << root-> oper << " " << root->a << " " << root->b << '\n';
+	std::cout << root << ": " << root-> oper << ' ' << root->a << ' ' << root->b << '\n';
 	if(root->oper == 0)
 		return;
 	printRoots((node *)root->a);
 	printRoots((node *)root->b);	
 }
 
-void print(std::deque<bool> & input,const std::vector< std::vector<bool> > & output,const std::vector<node *> & root){
+void print(	std::deque<bool> & input,
+			const std::vector< std::vector<bool> > & output,
+			const std::vector<node *> & root){
 	for(int i = 0; i < (1 << input.size()); i++){
 		for(size_t j = 0; j < input.size(); j++)
 			input[j] = ((i >> j) & 1);
 		for(int j = input.size() - 1; j >= 0; j--)
 			std::cout << input[j];
 		for(int j = input.size() - 1; j >= 0; j--)
-			std::cout << " E: " << output[j][i] << " S: " << exec(*(root[j]));
+			std::cout << " I: " << exec(*(root[j])) << " O: " << output[j][i];
 		std::cout << '\n';
 	}
 }
-
-
 
 void printData(std::deque<bool> & input,const std::vector< std::vector<bool> > & output,const std::vector<node *> & root,const std::set<weighted_pointer> & gate,std::vector<bool> & perfect){
 	std::cout << "BEST FITNESS: " << fitness(root,input,output,gate,perfect) << '\n';
