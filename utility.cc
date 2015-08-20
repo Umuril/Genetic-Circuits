25be37532835485d03914ff39280205ed091a938
Umuril <ed.rubi.1993@hotmail.com>
2015-08-20 19:50:21 -0300

    Update exec and fitness functions

--- utility.cc
+++ utility.cc
@@ -1,30 +1,30 @@
 #include "utility.h"
 
-bool exec(node root){
+bool exec(node root,std::deque<bool> & input,std::vector<node> & gate){
 	switch(root.oper){
 		case 0: // 000  NOTHING
-			return *((bool *)(root.a));
+			return input[root.a];
 			break;
 		case 1: // 001  AND
-			return exec(*((node *)(root.a))) & exec(*((node *)(root.b)));
+			return exec(gate[root.a],input,gate) & exec(gate[root.b],input,gate);
 			break;
 		case 2: // 010  OR
-			return exec(*((node *)(root.a))) | exec(*((node *)(root.b)));
+			return exec(gate[root.a],input,gate) | exec(gate[root.b],input,gate);
 			break;
 		case 3: // 011  XOR
-			return exec(*((node *)(root.a))) ^ exec(*((node *)(root.b)));
+			return exec(gate[root.a],input,gate) ^ exec(gate[root.b],input,gate);
 			break;
 		case 4: // 100  NOT
-			return (bool)~(*((bool *)root.a));
+			return ~exec(gate[root.a],input,gate);
 			break;
 		case 5: // 101  NAND
-			return (bool)~(exec(*((node *)(root.a))) & exec(*((node *)(root.b))));
+			return ~(exec(gate[root.a],input,gate) & exec(gate[root.b],input,gate));
 			break;
 		case 6: // 110  NOR
-			return (bool)~(exec(*((node *)(root.a))) | exec(*((node *)(root.b))));
+			return ~(exec(gate[root.a],input,gate) | exec(gate[root.b],input,gate));
 			break;
 		case 7: // 111  XNOR
-			return (bool)~(exec(*((node *)(root.a))) ^ exec(*((node *)(root.b))));
+			return ~(exec(gate[root.a],input,gate) ^ exec(gate[root.b],input,gate));
 			break;
 		default:
 			return false;
@@ -32,28 +32,17 @@ bool exec(node root){
 	}
 }
 
-int fitness(const std::vector<node *> &root,
-			std::deque<bool> & input,
-			const std::vector< std::vector<bool> > &output,
-			const std::set<weighted_pointer> & gate,
-			std::vector<bool> & perfect){
-				
+int fitness(std::vector<node> &gate, std::vector<size_t> &root, const size_t &input_lenght,const std::vector< std::vector<bool> > &output){
+	std::deque<bool> input(input_lenght);
 	int solution = 0;
-	for_each(gate.begin(),gate.end(),[](weighted_pointer const & w){w.weight = 0;});
-	std::fill(perfect.begin(),perfect.end(),true);
+	
 	for(int i = 0; i < (1 << input.size()); i++){
 		for(size_t j = 0; j < input.size(); j++)
 			input[j] = ((i >> j) & 1);
-		for(size_t j = 0; j < root.size(); j++){
-			if(exec(*(root[j])) == output[j][i])
+		for(size_t j = 0; j < root.size(); j++)
+			if(exec(gate[root[j]],input,gate) == output[j][i])
 				solution += 100;
-			else
-				perfect[j] = false;
-		}	
 	}
-	for(auto &&item_perfect : perfect)
-		if(item_perfect)
-			solution += 100;
 	
-	return solution - gate.size() + input.size();
+	return solution - gate.size() + input_lenght;
 }
