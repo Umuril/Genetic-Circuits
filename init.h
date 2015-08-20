3d7ad3b6beecf29285434bae0978985d92a44c29
Umuril <ed.rubi.1993@hotmail.com>
2015-08-20 20:00:38 -0300

    Update init.h

--- init.h
+++ init.h
@@ -4,13 +4,7 @@
 #include <deque>		// std::deque
 #include <fstream>		// std::ifstream
 #include <vector>		// std::vector, std::vector<bool>
-#include "structs.h"	// struct node, struct weighted_pointer
 
-void init(	std::deque<bool> &input,
-			const std::vector< std::vector<bool> > &output,
-			std::set<weighted_pointer> &gate,
-			std::vector<node *> &root);
-bool read_output_from_file(	std::vector< std::vector<bool> > &output,
-							size_t &input_lenght);
+bool read_output_from_file(	std::vector< std::vector<bool> > &output, size_t &input_lenght);
 
 #endif
