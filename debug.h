2d5cebc530f1a936f6e4e10732f480b99e61a424
Umuril <ed.rubi.1993@hotmail.com>
2015-08-20 20:00:11 -0300

    Update debug.h

--- debug.h
+++ debug.h
@@ -5,16 +5,11 @@
 #include <iostream>		// std::cout
 #include <vector>		// std::vector,std::vector<bool>
 #include "structs.h"	// struct node
-#include "utility.h"	// exec(), fitness()
+#include "utility.h"
 
-void print(	std::deque<bool> &input,
-			const std::vector< std::vector<bool> > &output,
-			const std::vector<node *> &root);
-void printData(	std::deque<bool> &input,
-				const std::vector< std::vector<bool> > &output,
-				const std::vector<node *> &root,
-				const std::set<weighted_pointer> &gate,
-				std::vector<bool> &perfect);
-void printRoots(const node *root);
+void print(std::vector<node> &gate, std::vector<size_t> &root,const size_t &input_lenght,const std::vector< std::vector<bool> > &output);
+void printData(std::vector<node> &gate, std::vector<size_t> &root,const size_t & input_lenght,const std::vector< std::vector<bool> > &output);
+void printGate(const std::vector<node> &gate);
+void printRoot(const std::vector<size_t> &root);
 
 #endif
