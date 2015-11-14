e5fbfe54ca5ceb890b0bd0bf74c084a78b82d19c
Eddy <ed.rubi.1993@gmail.com>
2015-11-14 09:31:44 +0100

    Change mutation.cc to work with multi-threading

--- mutation.cc
+++ mutation.cc
@@ -2,86 +2,64 @@
 #include "debug.h"
 #include <iostream>
 
-void changeRoots(	std::vector<node *> &root,
-					std::set<weighted_pointer> &gate){
-	
-	for(auto &&item_root : root){
-		auto it = std::find_if(gate.begin(),gate.end(),[&](const weighted_pointer & tmp){return tmp.pointer.get() == item_root;});
-		if(it->weight == 0 && (rand() % 2)){
-			node * actualRoot = item_root;
-			std::set<weighted_pointer>::iterator at;
-			do
-				at = next(gate.begin(),rand() % gate.size());
-			while(actualRoot == at->pointer.get());
-			item_root = at->pointer.get();
-		}
-	}
+void changeRoots(std::vector<node> &gate, std::vector<size_t> &root){
+    for(auto &&item_root : root){
+        size_t new_root;
+        do
+            new_root = rand() % gate.size();
+        while(item_root == new_root);
+        item_root = new_root;
+    }
 }
 
-void randomize(	std::set<weighted_pointer> &gate,
-				std::deque<bool> &input,
-				std::vector<node *> &root){
-	
+void randomize(std::vector<node> &gate, std::vector<size_t> &root, const size_t &input_lenght){
+
 	unsigned short int random;
 	random = rand() % 100;
-	if(random <= 90)
-		tryAdd(gate);
-	else
-		tryRemove(gate,input,root);
-	if(random <= 100)
-		changeRoots(root,gate);
-	return;	
-}
 
-void tryAdd(std::set<weighted_pointer> &gate){
-	weighted_pointer addpointer;
-	int random = rand() % 7 + 1;
-	int randomA = rand() % gate.size();
-	auto A = gate.begin();
-	std::advance(A,randomA);
-	if(random == 4){
-		addpointer.pointer = std::make_unique<node>(node{(unsigned int)random,A->pointer.get(),nullptr});
+	if(random <= 75){
+        //std::cout << "ADD: ";
+		//printGate(gate);
+		tryAdd(gate,input_lenght);
 	}else{
-		int randomB;
-		do
-			randomB = rand() % gate.size();
-		while(randomA == randomB);
-		auto B = gate.begin();
-		advance(B,randomB);
-		addpointer.pointer = std::make_unique<node>(node{(unsigned int)random,A->pointer.get(),B->pointer.get()});
+        //std::cout << "REMOVE: ";
+		//printGate(gate);
+		tryRemove(gate,input_lenght,root);
 	}
-	addpointer.weight = 0;
-	gate.emplace(std::move(addpointer));
+	if(random <= 100)
+		changeRoots(gate,root);
 	return;
 }
 
-void tryRemove(	std::set<weighted_pointer> &gate,
-				std::deque<bool> &input,
-				std::vector<node *> &root){
-					
-	if(gate.size() <= input.size())
-		return;
-	std::set<weighted_pointer>::iterator at;
-	do
-		at = std::next(gate.begin(),rand() % gate.size());
-	while((at->pointer)->oper == 0);
-	
-	if(at->weight > 0)
-		return;
-	
-	node * tmp_pointer = at->pointer.get();
-	
-	for(auto &&item_gate : gate){
-		if((node *)(item_gate.pointer)->a == tmp_pointer)
-    		item_gate.pointer->a = tmp_pointer -> a;
-    	if((item_gate.pointer)->oper != 4)
-    		if((node *)(item_gate.pointer)->b == tmp_pointer)
-    			item_gate.pointer->b = tmp_pointer -> a;
-	}
-	
-	for(auto &&item_root : root)
-    	if(item_root == tmp_pointer)
-    		item_root = (node*)(tmp_pointer -> a);
-	
-	gate.erase(at);
+void tryAdd(std::vector<node> &gate, const size_t & input_lenght){
+    int random = rand() % 7 + 1;
+    int randomA = rand() % gate.size();
+    int randomB = rand() % gate.size();
+    gate.emplace_back(node{random,randomA,randomB});
+	return;
 }
+
+void tryRemove(std::vector<node> &gate, const size_t & input_lenght, std::vector<size_t> &root){
+
+	if(gate.size() <= input_lenght)
+		return;
+
+    size_t pt = (rand() % (gate.size()-input_lenght))+input_lenght;
+
+    //printGate(gate);
+    //std::cout << "PT: " << pt;
+
+    for(int i = input_lenght; i < gate.size(); i++){
+        if(gate[i].a >= pt)
+            gate[i].a--;
+        if(gate[i].b >= pt)
+            gate[i].b--;
+    }
+
+
+    gate.erase(gate.begin()+pt);
+
+    //std::cout << " - AFTER    ";
+    //printGate(gate);
+}
+
